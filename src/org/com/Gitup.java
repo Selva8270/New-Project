c73b6ac84c540fe96ab863126b19f01d4b100092
Selva8270 <selvakumarg3649@gmail.com>
2024-01-04 11:52:15 +0530

    Hi..selva

---
+++ src/org/com/Gitup.java
@@ -0,0 +1,19 @@
+package org.com;
+
+public class Gitup {
+	
+	public void saving() {
+		System.out.println(" My account Savings");
+		}
+		public void fixed() {
+		System.out.println(" My amount is Fixed");
+		}
+		
+		
+		public static void main(String[] args) {
+			Gitup g=new Gitup();
+			g.saving();
+			g.fixed();
+		}
+
+}
